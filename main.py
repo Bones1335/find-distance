8b90588c477f0ef06fca9be39a93c2e867e21163
Alexander Hansen <ahansen1335@gmail.com>
2024-12-14 10:53:51 +0100

    add default option

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import csv
 import sys
 
 file = sys.argv[1]
+default_start_city = ('25000', 'BESANCON')
 
 with open(file, newline="") as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
-        print(row["zip_code_1"], row["city_1"])
-        print(row["zip_code_2"], row["city_2"])
-        print(row["destination_zip_code"], row["destination_city"])
+        city_1 = (row["zip_code_1"], row["city_1"])
+        city_2 = (row["zip_code_2"], row["city_2"])
+        destination_city = (row["destination_zip_code"], row["destination_city"])
+        print(default_start_city)
+        print(city_1)
+        print(city_2)
+        print(destination_city)
 
