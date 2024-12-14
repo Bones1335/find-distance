d6435928cb236d0ad8fd7cb97eec23e1b5c44c2b
Alexander Hansen <ahansen1335@gmail.com>
2024-12-14 10:26:57 +0100

    open csv file from command line argument and print each line's content

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import csv
 import sys
 
-file = open(sys.argv[1])
+file = sys.argv[1]
 
-for line in file:
-    print(line)
+with open(file, newline="") as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row["zip_code_1"], row["city_1"])
+        print(row["zip_code_2"], row["city_2"])
+        print(row["destination_zip_code"], row["destination_city"])
+
