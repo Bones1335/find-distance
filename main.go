84ba6640f4f32ae1bfdb0bb0da7051b0644683fc
Alexander Hansen <ahansen1335@gmail.com>
2025-09-18 06:44:49 +0200

    check which starting city is closest to the destination city to calculate the shortest distance

--- main.go
+++ main.go
@@ -22,9 +22,7 @@ func main() {
 	apiKey := os.Getenv("API_KEY")
 
 	args := os.Args
-	defaultCity := args[1:3]
-	fmt.Println(defaultCity)
-
+	defaultCity := url.PathEscape(args[1] + " " + args[2])
 	fileName := args[3]
 
 	file, err := os.Open(fileName)
@@ -47,31 +45,94 @@ func main() {
 
 		currentCity := url.PathEscape(internship.CurrentZipCode + " " + internship.CurrentCity)
 
+		destinationCity := url.PathEscape(internship.DestinationZipCode + " " + internship.DestinationCity)
+
+		if defaultCity == fixedCity || defaultCity == currentCity || (defaultCity == fixedCity && defaultCity == currentCity) {
+
+			lon1, lat1, err := api.GetGeocodeRequest(apiKey, defaultCity)
+			if err != nil {
+				fmt.Printf("error requesting data: %+v", err)
+				return
+			}
+
+			lon2, lat2, err := api.GetGeocodeRequest(apiKey, destinationCity)
+			if err != nil {
+				fmt.Printf("error requesting data: %+v", err)
+				return
+			}
+
+			cities := [][]float64{{lon1, lat1}, {lon2, lat2}}
+
+			dist, err := api.PostDirectionsRequest(apiKey, cities)
+			if err != nil {
+				fmt.Println("Error getting distance", err)
+				return
+			}
+
+			calculatedDistances = append(calculatedDistances, api.Distance{
+				LastName:  internship.LastName,
+				FirstName: internship.FirstName,
+				Distance:  ((dist / 1000) * 2),
+			})
+
+			fmt.Printf("%s\n", defaultCity)
+
+			continue
+		}
+
 		lon1, lat1, err := api.GetGeocodeRequest(apiKey, fixedCity)
 		if err != nil {
-			fmt.Printf("error requesting data: %+v", err)
+			fmt.Printf("error requesting data: %+v\n", err)
 			return
 		}
 
 		lon2, lat2, err := api.GetGeocodeRequest(apiKey, currentCity)
 		if err != nil {
-			fmt.Printf("error requesting data: %+v", err)
+			fmt.Printf("error requesting data: %+v\n", err)
 			return
 		}
 
-		cities := [][]float64{{lon1, lat1}, {lon2, lat2}}
+		lon3, lat3, err := api.GetGeocodeRequest(apiKey, destinationCity)
+		if err != nil {
+			fmt.Printf("error requesting data: %+v\n", err)
+			return
+		}
 
-		dist, err := api.PostDirectionsRequest(apiKey, cities)
+		fixedDestination := [][]float64{{lon1, lat1}, {lon3, lat3}}
+		currentDestination := [][]float64{{lon2, lat2}, {lon3, lat3}}
+
+		dist1, err := api.PostDirectionsRequest(apiKey, fixedDestination)
 		if err != nil {
 			fmt.Println("Error getting distance", err)
 			return
 		}
 
-		calculatedDistances = append(calculatedDistances, api.Distance{
-			LastName:  internship.LastName,
-			FirstName: internship.FirstName,
-			Distance:  ((dist / 1000) * 2),
-		})
+		dist2, err := api.PostDirectionsRequest(apiKey, currentDestination)
+		if err != nil {
+			fmt.Println("Error getting distance", err)
+			return
+		}
+
+		if dist1 < dist2 {
+			calculatedDistances = append(calculatedDistances, api.Distance{
+				LastName:  internship.LastName,
+				FirstName: internship.FirstName,
+				Distance:  ((dist1 / 1000) * 2),
+			})
+
+			fmt.Printf("%s\n", fixedCity)
+
+		} else {
+
+			calculatedDistances = append(calculatedDistances, api.Distance{
+				LastName:  internship.LastName,
+				FirstName: internship.FirstName,
+				Distance:  ((dist2 / 1000) * 2),
+			})
+
+			fmt.Printf("%s\n", currentCity)
+
+		}
 	}
 
 	createCsv.CreateCsv(calculatedDistances)
